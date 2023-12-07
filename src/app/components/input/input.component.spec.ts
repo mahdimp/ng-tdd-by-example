987be7447a129721dd444ca4c3204c5400a45548
Mahdi Madah Pour <mahdi.mp@gmail.com>
2023-12-07 15:22:52 +0330

    Write first test

---
+++ src/app/components/input/input.component.spec.ts
@@ -0,0 +1,32 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InputComponent } from './input.component';
+
+describe('InputComponent', () => {
+  let component: InputComponent;
+  let fixture: ComponentFixture<InputComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ InputComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(InputComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render the provided input value', () => {
+    const element = fixture.debugElement.nativeElement as HTMLElement;
+    const message = 'Hello';
+    component.message = message;
+    fixture.detectChanges();
+    console.log(fixture.debugElement.nativeElement);
+    expect(element.innerHTML).toBe(message)
+  })
+});
